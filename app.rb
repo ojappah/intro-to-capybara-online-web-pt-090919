bbe1e61a6309c69ea118fb5cb009dc066838e3a8
generous-vector-9581 <ojappah@gmail.com>
2020-01-02 01:53:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
-class Application < Sinatra::Base
-  # Old route from previous tests
-  get '/' do
-    erb :index
-  end
 
-  # New route to respond to the form submission
-  post '/greet' do
-    erb :greet
+class Application < Sinatra::Base
+  get '/' do		
+    erb :index		
+  end		
+  
+  post '/greet' do		
+    erb :greet		
   end
 end
